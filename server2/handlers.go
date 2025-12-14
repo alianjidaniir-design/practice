aae599e02a4dc27d1ddba715f5c5d0315e83385a
Mahdi Anjidani <>
2025-12-14 12:31:57 +0330

    branch

--- server2/handlers.go
+++ server2/handlers.go
@@ -3,12 +3,53 @@ package main
 import (
 	"fmt"
 	"log"
-	"strconv"
 	"net/http"
 	"strconv"
-
+	"strings"
 )
 
 const PORT = ":1234"
 
-func def
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("serving")
+	w.WriteHeader(http.StatusOK)
+	body := "Thanks for your request!\n"
+	fmt.Fprintf(w, body)
+}
+
+func deleteHandler(w http.ResponseWriter, r *http.Request) {
+	parmStr := strings.Split(r.URL.Path, "/")
+	fmt.Println("path", parmStr)
+	if len(parmStr) < 3 {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "404 not found")
+		return
+	}
+	log.Println("Serving:", r.URL.Path, "from", r.Host)
+	dataest := parmStr[2]
+	err := DElete(dataest)
+	if err != nil {
+		fmt.Println(err)
+		Body := err.Error() + "\n"
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "%s", Body)
+		return
+	}
+	body := dataest + "deleted\n!"
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s", body)
+}
+
+func listHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host)
+	w.WriteHeader(http.StatusOK)
+	body := list()
+	fmt.Fprintf(w, "%s", body)
+}
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Serving:", r.URL.Path, "from", r.Host)
+	w.WriteHeader(http.StatusOK)
+	body := fmt.Sprintf("Total entries: %d\n", len(data))
+	fmt.Fprintf(w, "%s", body)
+}
