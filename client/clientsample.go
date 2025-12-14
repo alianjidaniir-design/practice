4c2ec1773aa74d8f89829ec1fc9df38159a98133
Mahdi Anjidani <>
2025-12-14 08:09:18 +0330

    branch

---
+++ client/clientsample.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"bufio"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run client sample.go <file>")
+		return
+	}
+	f, err
+
+}
