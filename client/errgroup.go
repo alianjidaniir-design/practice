3769bc0ed5761642871de8c633b349206e47da17
Mahdi Anjidani <>
2025-12-16 10:16:57 +0330

    branch

--- client/errgroup.go
+++ client/errgroup.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run client.go url")
+		return
+	}
+	g := new(errgroup.Group)
+	for _, url := range os.Args[1:] {
+		url := url
+		g.Go(func() error {
+			resp, err := http.Get(url)
+			if err != nil {
+				return err
+			}
+			defer resp.Body.Close()
+			fmt.Println(url, "is OK")
+			return nil
+		})
+
+	}
+	err := g.Wait()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Everything is OK!")
+}
---
+++ client/go.mod
@@ -0,0 +1,5 @@
+module practice/client
+
+go 1.25.3
+
+require golang.org/x/sync v0.19.0
