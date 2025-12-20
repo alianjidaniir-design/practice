40b47516623a253ae9df9f6d73081a3505ec2efa
Mahdi Anjidani <>
2025-12-20 13:47:45 +0330

    branch

--- TCP/othertcps.go
+++ TCP/othertcps.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	args := os.Args
+	if len(args) <= 1 {
+		fmt.Println("Please enter a port number!")
+		return
+	}
+	SERVER := "localhost " + ":" + args[1]
+	s, err := net.Listen("tcp", SERVER)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+}
