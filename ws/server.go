3d1be5ca240122e063f7a0d9f84fd0b3a93ffa7e
Mahdi Anjidani <>
2025-12-21 09:33:56 +0330

    branch

--- ws/server.go
+++ ws/server.go
@@ -1,4 +1,4 @@
-package maint
+package main
 
 import (
 	"fmt"
@@ -19,4 +19,56 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-func main() {}
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to Go Web Server!\n")
+	fmt.Fprintf(w, "Please use /ws for Websocket!")
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Connection from:", r.Host)
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrader.Upgrade:", err)
+		return
+	}
+	defer ws.Close()
+
+	for {
+		mt, massage, err := ws.ReadMessage()
+		if err != nil {
+			log.Println("From", r.Host, "read:", err)
+			break
+		}
+		log.Println("Received", string(massage))
+		err = ws.WriteMessage(mt, massage)
+		if err != nil {
+			log.Println("WriteMessage:", err)
+			break
+		}
+	}
+}
+
+func main() {
+	arg := os.Args
+	if len(arg) > 1 {
+		PORT = ":" + arg[1]
+	}
+
+	mux := http.NewServeMux()
+	s := &http.Server{
+		Addr:         PORT,
+		Handler:      mux,
+		IdleTimeout:  5 * time.Second,
+		ReadTimeout:  2 * time.Second,
+		WriteTimeout: 2 * time.Second,
+	}
+	mux.HandleFunc("/", rootHandler)
+	mux.HandleFunc("/ws", wsHandler)
+	log.Println("Listening to TCP Port ", PORT)
+	err := s.ListenAndServe()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+}
