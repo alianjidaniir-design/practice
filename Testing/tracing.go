fe23705731f72ecc4671a0d499f780dd90f2c139
Mahdi Anjidani <>
2026-01-03 09:42:00 +0330

    ......

--- Testing/tracing.go
+++ Testing/tracing.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"runtime/trace"
+	"time"
+)
+
+func main() {
+	filepath := path.Join(os.TempDir(), "trace.out")
+	f, err := os.Create(filepath)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	err = trace.Start(f)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer trace.Stop()
+
+	for i := 0; i < 3; i++ {
+		s := make([]byte, 50000000)
+	}
+
+}
