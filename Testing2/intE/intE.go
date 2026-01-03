25ba7dfb4f5ac30ddb88a05c400d6e3445a65845
Mahdi Anjidani <>
2026-01-03 18:13:27 +0330

    branch

---
+++ Testing2/intE/intE.go
@@ -0,0 +1,9 @@
+package intRE
+
+import "regexp"
+
+func matchInt(s string) bool {
+	t := []byte(s)
+	re := regexp.MustCompile(`^[-+]?\d+$`)
+	return re.Match(t)
+}
---
+++ Testing2/intE/test.go
@@ -0,0 +1,34 @@
+package intRE
+
+import (
+	"math/rand"
+	"strconv"
+	"testing"
+)
+
+func random(min, max int) int {
+	return rand.Intn(max-min) + min
+}
+
+func TestMatchInt(t *testing.T) {
+	if matchInt("") {
+		t.Error(`matchInt("") != false`)
+	}
+	if matchInt("00") == false {
+		t.Error(`matchInt(+00) != true`)
+	}
+	if matchInt("-00") == false {
+		t.Error(`matchInt(-00) != true`)
+	}
+	if matchInt("+00") == false {
+		t.Error(`matchInt(+00) != true`)
+	}
+}
+
+func TestWithRandom(t *testing.T) {
+	n := strconv.Itoa(random(-123456, 2345678))
+	if matchInt(n) == false {
+		t.Error("n =", n)
+	}
+
+}
