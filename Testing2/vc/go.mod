1760acf260cc60cc35f658c901643bcd5add60bc
Mahdi Anjidani <>
2026-01-06 12:49:38 +0330

    branch

---
+++ Testing2/vc/go.mod
@@ -0,0 +1,5 @@
+module practice/Testing2/vc
+
+go 1.25.3
+
+require golang.org/x/text v0.32.0
--- Testing2/vc/vul.go
+++ Testing2/vc/vul.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"fmt"
+
+	"golang.org/x/text/language"
+)
+
+func main() {
+	greece := language.Make("el")
+	en := language.Make("en")
+	fmt.Println(greece.Region())
+	fmt.Println(en.Region())
+
+}
