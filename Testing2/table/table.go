d25165a39f33429dc22aee28f0cb862386f3c7bf
Mahdi Anjidani <>
2026-01-06 10:43:06 +0330

    branch

--- Testing2/table/table.go
+++ Testing2/table/table.go
@@ -1,1 +1,9 @@
-package table
+package division
+
+func intDiv(a, b int) int {
+	return a / b
+}
+
+func floatDiv(a, b int) float64 {
+	return float64(a) / float64(b)
+}
--- Testing2/table/table_test.go
+++ Testing2/table/table_test.go
@@ -1,1 +1,35 @@
-package table
+package division
+
+import (
+	"testing"
+)
+
+type myTest struct {
+	a        int
+	b        int
+	resInt   int
+	resFloat float64
+}
+
+var tests = []myTest{
+	{a: 1, b: 2, resInt: 0, resFloat: 0.5},
+	{a: 7, b: 14, resInt: 0, resFloat: 0.5},
+	{a: 3, b: 3, resInt: 1, resFloat: 1.0},
+	{a: 6, b: 6, resInt: 1, resFloat: 1.0},
+	{a: 5, b: 2, resInt: 2, resFloat: 2.0},
+	{a: 5, b: 4, resInt: 1, resFloat: 1.2},
+}
+
+func TestAll(t *testing.T) {
+	t.Parallel()
+	for _, test := range tests {
+		intRes := intDiv(test.a, test.b)
+		if intRes != test.resInt {
+			t.Errorf("Expected %d, got %d", test.resInt, intRes)
+			floatResult := floatDiv(test.a, test.b)
+			if floatResult != test.resFloat {
+				t.Errorf("Expected %f, got %f", floatResult, test.resFloat)
+			}
+		}
+	}
+}
