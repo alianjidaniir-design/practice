2f49f041bfe6bc183c143f44644725459a008628
Mahdi Anjidani <>
2026-01-10 10:36:34 +0330

    branch

--- observability/samplePro.go
+++ observability/samplePro.go
@@ -1,11 +1,34 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"net/http"
-	"time"
+import "github.com/prometheus/client_golang/prometheus"
 
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
-)
+var PORT = ":1234"
+var counter = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Namespace: "Ali",
+		Name:      "my_counter",
+		Help:      "This is my counter ",
+	})
+
+var gauge = prometheus.NewGauge(
+	prometheus.GaugeOpts{
+		Namespace: "Ali",
+		Name:      "my_gauge",
+		Help:      "This is my gauge ",
+	})
+var histogram = prometheus.NewHistogram(
+	prometheus.HistogramOpts{
+		Namespace: "Ali",
+		Name:      "my_histogram",
+		Help:      "This is my histogram ",
+	})
+var summery = prometheus.NewSummary(
+	prometheus.SummaryOpts{
+		Namespace: "Ali",
+		Name:      "my_summary",
+		Help:      "This is my summary ",
+	})
+
+func main() {
+
+}
