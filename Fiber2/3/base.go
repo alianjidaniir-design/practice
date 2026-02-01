f50bf216f377a23f6bd6467e0bae6d450cbf0b6f
Mahdi Anjidani <>
2026-02-01 09:37:02 +0330

    branch

--- Fiber2/3/base.go
+++ Fiber2/3/base.go
@@ -1,34 +1,21 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/basicauth"
 )
 
 func main() {
 
 	app := fiber.New()
+	micro := fiber.New()
+	app.Static("/", "./rep")
 
-	app.Use(basicauth.New(basicauth.Config{
-		Users: map[string]string{
-			"john":  "doe",
-			"admin": "123456",
-		},
-		Realm: "always",
-		Authorizer: func(user, pass string) bool {
-			if user == "john" && pass == "doe" {
-				return true
-			}
-			if user == "admin" && pass == "123456" {
-				return true
-			}
-			return false
-		},
-		Unauthorized: func(c *fiber.Ctx) error {
-			return c.SendFile("./hello.html")
-		},
-		ContextUsername: "_user",
-		ContextPassword: "_pass",
-	}))
-	app.Listen(":3000")
+	app.Mount("/mount", micro)
+	micro.Get("/doe", func(c *fiber.Ctx) error {
+		return c.SendStatus(fiber.StatusOK)
+	})
+
+	log.Fatal(app.Listen(":3000"))
 }
